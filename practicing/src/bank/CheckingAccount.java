88f209d21cf3bb3b49ec7071c2d0a837f746db5e
Isaac Inacio <isaakinf@gmail.com>
2026-03-16 21:05:18 -0300

    refactor: ajuste do método de saque na conta corrente

--- practicing/src/bank/CheckingAccount.java
+++ practicing/src/bank/CheckingAccount.java
@@ -2,7 +2,8 @@ package bank;
 
 public class CheckingAccount extends Account {
 
-    Double limit = 2.000;
+    private double limit = 2.000;
+    private final double fee = 2.0;
 
     public CheckingAccount(Double balance, String holder) {
         super(balance, holder);
@@ -10,11 +11,21 @@ public class CheckingAccount extends Account {
 
     @Override
     public boolean withdraw(double amount) {
-        if(this.balance >= amount){
-            this.balance -= amount;
+        double totalWithdraw = amount + fee;
+
+        if (this.balance >= totalWithdraw) {
+            this.balance -= totalWithdraw;
             return true;
-        } else {
-            System.out.println("Insufficient funds!");
+        } else if (this.balance + this.limit >= totalWithdraw) {
+            double neededFromLimit = totalWithdraw - this.balance;
+
+            this.balance = 0.0;
+            this.limit -= neededFromLimit;
+
+            System.out.println("Using the special limit...");
+            return true;
+        }else {
+            System.out.println("Insufficient funds (Limit exceeded)!");
             return false;
         }
     }
